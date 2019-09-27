addb8642b78be593f3a2d13b072670ed956f8412
Daniel Bright <brahyt@gmail.com>
2019-09-27 11:32:00 -0700

    working part 1 starting part 2

--- index.js
+++ index.js
@@ -4,33 +4,43 @@ function getDogImage(userNum) {
   fetch(`https://dog.ceo/api/breeds/image/random/${userNum}`)
     .then(response => response.json())
     .then(responseJson => 
-      displayResults(responseJson))
+      displayResults(responseJson.message))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
 function validUserNum(val) {
-    return (val>=1 && val<=50);
+    return (val>=1 && val <=50);
 }
 
 function takeInfo() {
-    let userNum = $("input[type=number]").val()
-    console.log(userNum);
+  let userNum = $("input[type=number]").val()
+  return userNum
+}
+
+function snippitBuilder(responseJson){
+  let result = ''
+  responseJson.forEach(ele => result += `<img src="${ele}" class="results-img">`)
+  return result;
+}
+
+function displayInConsole(arrayOfUrl){
+  arrayOfUrl.forEach(console.image(arrayOfUrl))
 }
 
 function displayResults(responseJson) {
-  console.log(responseJson);
-  //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
-  )
-  //display the results section
+  $('.results-img').replaceWith(snippitBuilder(responseJson));
   $('.results').removeClass('hidden');
 }
-
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    takeInfo();
+    let userInfo = takeInfo()  //datatype is 'number' from user
+    if (validUserNum(userInfo)) {
+      getDogImage(userInfo);   //asking for a 'number' of images from API
+    }
+    else {
+      console.log('notworking')
+    }
   });
 }
 
